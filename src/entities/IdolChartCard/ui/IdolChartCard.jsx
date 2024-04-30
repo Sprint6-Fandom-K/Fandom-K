8a45728cee23d56cab152d8e33f9589448d53de5
codefug <robot9917@naver.com>
2024-05-01 08:26:35 +0900

    feat: complete SortChart Layout

--- src/entities/IdolChartCard/ui/IdolChartCard.jsx
+++ src/entities/IdolChartCard/ui/IdolChartCard.jsx
@@ -1,3 +1,4 @@
-export const IdolChartCard = ({ children }) => {
-	return <div>{children}</div>;
+export const IdolChartCard = ({ item }) => {
+	const { group, name, profilePicture, totalVotes } = item;
+	return <div>{group + name + profilePicture + totalVotes}</div>;
 };
