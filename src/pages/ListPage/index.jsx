044886f675b723defbf6b642fc8cf69034a8fe09
codefug <robot9917@naver.com>
2024-05-02 11:43:46 +0900

    fix: merge for ListPage

--- src/pages/ListPage/index.jsx
+++ src/pages/ListPage/index.jsx
@@ -1,1 +1,19 @@
-export { ListPage } from "./ui/ListPage";
+import {
+	MainContainer,
+	MainContentContainer,
+} from "@/shared/Container/Container";
+import MonthChart from "@/widgets/MonthChart";
+
+export default function ListPage() {
+	return (
+		// 이미지도 하나 넣어야 함 img_top design
+		// header
+		<MainContainer>
+			<MainContentContainer>
+				{/* 크레딧 widget */}
+				{/* 조공 widget */}
+				<MonthChart />
+			</MainContentContainer>
+		</MainContainer>
+	);
+}
