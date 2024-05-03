090fc9b20459020ae30e46dd030abf001fd41d41
codefug <robot9917@naver.com>
2024-05-03 15:38:18 +0900

    feat(SortChart): split SelectGender and change infinite scroll logic in SortChart

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -2,9 +2,8 @@ import { BASE_URL } from "../constant/constant";
 
 export async function getCharts({ gender, cursor }) {
 	const URL = `${BASE_URL}/charts/{gender}`;
-	const query = new URLSearchParams({ gender, cursor });
+	const query = new URLSearchParams({ gender, cursor: cursor ?? 0 });
 	const response = await fetch(`${URL}?${query.toString()}`);
-
 	const data = await response.json();
 	const nextCursor = data.nextCursor;
 	const idols = data.idols;
