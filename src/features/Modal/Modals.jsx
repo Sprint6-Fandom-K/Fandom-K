c374b4b1234bc295ddc103bd85c8f638362bcf59
JustDevRae <ksrae165@gmail.com>
2024-05-05 00:45:49 +0900

    feat: 크레딧 충전하기 모달 레이아웃 구현

--- src/features/Modal/Modals.jsx
+++ src/features/Modal/Modals.jsx
@@ -1,9 +1,23 @@
 import { createPortal } from "react-dom";
 import { useState } from "react";
-import Credit from "@/shared/asset/CreditImage.svg";
+import Credit from "@/shared/asset/Credit.svg";
 import Close from "@/shared/asset/CloseX.svg";
 import styled from "styled-components";
 
+// const NoCreditModal = styled.div`
+// 	position: absolute;
+// 	top: 45%;
+// 	left: 50%;
+// 	display: flex;
+// 	flex-direction: column;
+// 	align-items: center;
+// 	gap: 31px;
+// 	width: 339px;
+// 	height: 331px;
+// 	transform: translate(-50%, -50%);
+// 	border-radius: 12px;
+// 	background-color: #181D26;
+// `;
 
 const CloseButton = styled.button`
 	position: absolute;
@@ -29,13 +43,6 @@ const CommonButton = styled.button`
 	cursor: pointer;
 `;
 
-const CreditImg = styled.div`
-	background-color: aqua;
-	width: 113px;
-	height: 113px;
-	margin-top: 56px ;
-`;
-
 const Text = styled.p`
 	color: #fff;
 	font-size: 16px;
@@ -46,60 +53,55 @@ const Text = styled.p`
 	}
 `;
 
-const NoCreditModal = styled.div`
-	position: absolute;
-	top: 45%;
-	left: 50%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 31px;
-	width: 339px;
-	height: 331px;
-	transform: translate(-50%, -50%);
-	border-radius: 12px;
-	background-color: #181D26;
-`;
-
 const RadioWrapper = styled.div`
   display: flex;
   flex-direction: column;
-	margin-top: 70px;
 	align-items: center;
 	gap: 8px;
+	margin-top: 70px;
+`;
+
+const ValueWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 6.5px;
 `;
 
 const RadioLabel = styled.label`
-  background-color: #020003;
+	box-sizing: border-box;
+	padding: 18px 20px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 	width: 295px;
 	height: 62px;
   border-radius: 8px;
 	border: 1px solid #f7f7f8;
+  background-color: #020003;
 	color: #fff;
 	font-size: 20px;
 	font-weight: 700;
-	display: flex;
-	align-items: center;
-	gap: 183px;
 `;
 
 const CustomRadio = styled.input.attrs({ type: 'radio' })`
-	appearance: none;
-
+	appearance: none; // 기본 브라우저에서 기본 스타일 제거
 	display: inline-block;
 	width: 16px;
 	height: 16px;
-
-	background-color: #fff;
-	border: 2px solid #8C92AB;
+	box-shadow: 0 0 0 1px #8c92ab; // radio 버튼 테두리
+	border: 2px solid #fff; // 테두리와 원 사이의 색상
+	background-color: #8c92ab; // radio 버튼 내부 원 색상
 	border-radius: 50%;
 
+	// 체크될 시에, 변화되는 스타일 설정
 	&:checked{
-		border: 2px solid #F96D69;
+		box-shadow: 0 0 0 1px #f96d69;
+		border: 2px solid #fff;
+		background-color: #f96d69;
 	}
 `;
 
-const ChargeCreditModal = styled.div`
+const ChargeModal = styled.div`
 	position: absolute;
 	top: 45%;
 	left: 50%;
@@ -121,7 +123,6 @@ const ChargeCreditModal = styled.div`
 		position: absolute;
 		top: 25px;
 		left: 17px;
-
 		font-size: 18px;
 		font-weight: 600;
 		line-height: 21px;
@@ -151,20 +152,21 @@ const RadioModal = ({ options, onClose, onSelect }) => {
 	const handleCharge = () => {
 		onClose(false);
 		console.log("모달창 닫고 크레딧 충전");
+		console.log(selectedOption);
 	}
 
 	return (
 		<Overlay>
-			<ChargeCreditModal>
+			<ChargeModal>
 				<Text>크레딧 충전하기</Text>
-				<CloseButton onClick={onClose}>
-					<img src={Close} alt="닫기" />
-				</CloseButton>
-
+				<CloseButton onClick={onClose}><img src={Close} alt="닫기" /></CloseButton>
 				<RadioWrapper>
 					{options.map((option, index) => (
 						<RadioLabel key={index}>
-							{option.label}
+							<ValueWrapper>
+								<img src={Credit} alt="크레딧"/>
+								{option.label}
+							</ValueWrapper>
 							<CustomRadio
 								type="radio"
 								value={option.value}
@@ -177,12 +179,12 @@ const RadioModal = ({ options, onClose, onSelect }) => {
 				<CommonButton onClick={handleCharge}>
 					충전하기
 				</CommonButton>
-			</ChargeCreditModal>
+			</ChargeModal>
 		</Overlay>
 	);
 }
 
-export default function Modals() {
+export default function Modals() { // ListPage 라고 가정
 
 	const options = [
 		{ label: '100', value: 100 },
@@ -220,5 +222,3 @@ export default function Modals() {
 		</>
 	);
 }
-
-
