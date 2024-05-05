8005d822ee09f8fd1f58235f8132d99b11072845
JustDevRae <ksrae165@gmail.com>
2024-05-05 17:22:47 +0900

    fix: 코드수정

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,7 @@
 import LandingPage from "@/pages/LandingPage/ui/LandingPage";
-import Modals from "../features/Modal/Modals";
 
 export default function App() {
 	return (
-		// <LandingPage/>
-		<Modals />
+		<LandingPage/>
 	);
 }
